385c265e3340ccbddecc6443aee241d4db29c568
kevinzhao2233 <2939575326@qq.com>
2020-04-04 20:24:37 +0800

    【feature】完成搜索-专辑页布局

--- components/common/AlbumList.vue
+++ components/common/AlbumList.vue
@@ -11,6 +11,7 @@
         }"
       ></div>
       <span class="name" :title="item.name">{{ item.name }}</span>
+      <span class="artist" :title="item.artist.name">{{ item.artist.name }}</span>
     </li>
   </ul>
 </template>
@@ -34,20 +35,55 @@ export default {
 .k-list-box {
   display: grid;
   justify-content: space-between;
-  grid-template-columns: repeat(auto-fill, 130px);
+  grid-template-columns: repeat(auto-fill, 150px);
+  padding: 0 24px;
   grid-gap: 24px;
   width: 100%;
   color: $mid-10;
 
   .item {
     margin: 0 0 12px;
-    width: 130px;
+    width: 150px;
     height: 190px;
-    text-align: center;
 
     .img {
+      position: relative;
       width: 130px;
       height: 130px;
+
+      &::after {
+        content: '';
+        display: block;
+        width: 150px;
+        height: 130px;
+        background: url('~static/img/album_cover.png') no-repeat;
+        background-size: cover;
+      }
+
+      &:hover {
+        cursor: pointer;
+      }
+    }
+
+    .name {
+      display: inline-block;
+      margin-top: 8px;
+      width: 150px;
+      font-size: 16px;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      cursor: pointer;
+
+      &:hover {
+        color: $main-6;
+      }
+    }
+
+    .artist {
+      @extend .name;
+      margin-top: -2px;
+      font-size: 14px;
     }
   }
 }

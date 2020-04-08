8f0486cd93a8bd3872b36136b9f5336bd68cf283
kevinzhao2233 <2939575326@qq.com>
2020-04-08 11:37:03 +0800

    完善错误页面显示逻辑

---
+++ layouts/error.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <div class="content" v-if="error">
+      <h2>{{error.statusCode}}</h2>
+      <span>{{error.message}}</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ['error'],
+  mounted() {
+    console.log(this.error)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
+</style>
--- plugins/axios.js
+++ plugins/axios.js
@@ -1,27 +1,19 @@
-export default function({ $axios, redirect }) {
+export default function({ $axios, error }) {
   $axios.onRequest(config => {
     console.log('Making request to ' + config.url)
   })
 
-  $axios.onError(error => {
-    const code = parseInt(error.response && error.response.status)
-    switch (code) {
-      case 400:
-        console.log('状态码：400')
-        redirect('/400')
-        break
-      case 401:
-        console.log('状态码：401')
-        redirect('/401')
-        break
-      case 404:
-        console.log('状态码：404')
-        redirect('/404')
-        break
-      case 500:
-        console.log('状态码：500')
-        redirect('/500')
-        break
-    }
+  $axios.onError(err => {
+    const statusCode = parseInt(err.response && err.response.status)
+    const message = parseInt(err.response && err.response.data.code)
+    error({
+      statusCode,
+      message
+    })
+    // switch (code) {
+    //   case 400:
+    //     console.log('状态码：400 ━━(￣ー￣*|||━━')
+    //     // redirect('/400')    // 这中方式需要 pages 文件夹中有 400.vue, 所以采用上面的方法
+    //     break
   })
 }
